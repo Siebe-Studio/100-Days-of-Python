69e4ed7cf19c0f30d8c28700138a27b0ec684ea4
Siebee <siebe.burgerhout@icloud.com>
2023-06-06 21:09:37 +0200

    Revert "The beginnings!"
    This reverts commit 764d769039fa2d50674e523aca282cb2a6bd64d1.

--- D1 Band Name Generator/main.py
+++
@@ -1,4 +0,0 @@
-print("Welcome to Bandify!\nThe Best band name generator!")
-city = input("In what city did you grow up?\n")
-pet = input("What is the name of your most recent pet?\n")
-print(f"Your bandname could be {city} {pet}!")

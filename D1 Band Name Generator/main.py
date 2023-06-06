764d769039fa2d50674e523aca282cb2a6bd64d1
Siebee <siebe.burgerhout@icloud.com>
2023-06-06 21:01:55 +0200

    The beginnings!

---
+++ D1 Band Name Generator/main.py
@@ -0,0 +1,4 @@
+print("Welcome to Bandify!\nThe Best band name generator!")
+city = input("In what city did you grow up?\n")
+pet = input("What is the name of your most recent pet?\n")
+print(f"Your bandname could be {city} {pet}!")
---
+++ D1 Band Name Generator/readme.md
@@ -0,0 +1,7 @@
+# Day 1 Variables
+## Band name generator
+- Greets user.
+- Asks the user for the city they grew up in.
+- Asks the user for the name of their pet.
+- Combines the name of their city and the name of their pet.
+
